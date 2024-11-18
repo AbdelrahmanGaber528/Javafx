d02411051cb24b8e771db070afbe9d79159e1437
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-11-19 01:03:41 +0200

    add textfield

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,40 +1,46 @@
 package GUI;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 
 public class Main extends Application  {
-
+    Stage window;
     Button btn1 ;
-
+    Scene scene;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage mainWindow) {
+            window = mainWindow;
+            window.setTitle("Title of Window");
 
-            mainWindow.setTitle("Title of Window");
+            TextField nameInput = new TextField();
+
             btn1 = new Button();
             btn1.setText("Click Me");
-            btn1.setOnAction(e -> { //lambda expression
-                System.out.println("Hello from lambda expression.");
-                System.out.println("Lines on lambda is very amazing");
-            });
+            btn1.setOnAction(e ->  //lambda expression
+                System.out.println(nameInput.getText()));
 
-            StackPane layout = new StackPane();
-            layout.getChildren().add(btn1);
-            layout.setVisible(true);
+            VBox layout = new VBox();
+            layout.setPadding(new Insets(20,20,20,20));
+            layout.getChildren().addAll(nameInput,btn1);
 
-        Scene scene = new Scene(layout,300,250);
+        scene = new Scene(layout,300,250);
 
-        mainWindow.setScene(scene);
-        mainWindow.show();
+        window.setScene(scene);
+
+        window.show();
+
     }
 
 
